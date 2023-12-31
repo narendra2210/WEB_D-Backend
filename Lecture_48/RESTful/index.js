b6659e3e0024bfe116f40ea219ea2686c34e1df9
Narendra Singh <narendra.rocks2002@gmail.com>
2023-12-31 22:09:52 +0530

    WEB_D BACKEND

--- Lecture_48/RESTful/index.js
+++ Lecture_48/RESTful/index.js
@@ -31,13 +31,39 @@ app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname , 'views'));
 
 app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.urlencoded({extended:true}));//for form endcoded data
 
 app.get('/' , (req,res)=>{
     res.send("Root path pe aapka swagat h");
 })
 
+// Tastk - 1 Display all the blogs
 app.get('/blogs' , (req,res)=>{
     res.render('index' , {comments});
+})
+
+// Tastk - 2 Show just a form for adding new blog
+app.get('/blog/new' , (req,res)=>{
+    res.render('new');
+})
+
+// Tastk - 3 To add the new blog to Database 
+app.post('/blogs' , (req,res)=>{
+    // console.log(req.body);
+    // res.send("Chal gya");
+    let {username , Comment} = req.body;
+    comments.push({username , Comment , id:comments.length});
+    res.redirect('/blogs');
+})
+
+// Task - 4 To show info about 1 particular blog
+app.get('/blogs/:id' , (req,res)=>{
+    let {id} = req.params;
+    //console.log(id);
+    let foundComment = comments.find(Comment => Comment.id == id);
+    //console.log(foundComment);
+    //res.send("Show particular page");
+    res.render('show' , {foundComment});
 })
 
 app.listen(8080 , ()=>{
