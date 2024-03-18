b54e94f7397c9fe022ec24a8155e7d91b4efcbd2
Narendra Singh <narendra.rocks2002@gmail.com>
2024-03-18 19:34:00 +0530

    WEB_D BACKEND

---
+++ Lecture_62/github-project/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import GithubUser from './Components/GithubUser';
+import "./App.css"
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1 className='app' >App default functional component</h1>
+      <div className='parent'>
+        <GithubUser username="samarth0606" />
+        <GithubUser username="sabeelhps" />
+        <GithubUser username="narendra2210" />
+      </div>
+    </div>
+  )
+}
+
+export default App
