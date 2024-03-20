7221fa6e0fc588030876cd6a4ee0cf61f4639505
Narendra Singh <narendra.rocks2002@gmail.com>
2024-03-20 11:36:36 +0530

    WEB_D BACKEND

--- Lecture_63/QuotesApp/frontend/src/Components/mainNavigation/MainNavigation.js
+++ Lecture_63/QuotesApp/frontend/src/Components/mainNavigation/MainNavigation.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import styles from './MainNavigation.module.css'
+import styles from './MainNavigation.module.css' 
 
 const MainNavigation = () => {
   return (
     <nav className={styles.nav}>
-        <h2>Quote App</h2>
+        <h2>Quotes App</h2>
         <ul>
-            <li><Link to="/">All Quotes</Link></li>
-            <li><Link to="/new">New Quotes</Link></li>
+            <li> <Link to='/'> All Quotes </Link> </li>
+            <li> <Link to='/new'> New Quotes </Link> </li>
         </ul>
     </nav>
   )
--- Lecture_63/QuotesApp/frontend/src/Components/pages/AllQuotes.js
+++ Lecture_63/QuotesApp/frontend/src/Components/pages/AllQuotes.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Quote from '../Quote/Quote';
+
+
 
 const AllQuotes = () => {
+
+    let [quotes , setQuotes] = useState([]);
+
+    async function getQuotes(){
+        let res = await axios.get('http://localhost:8080/allquotes');
+        setQuotes(res.data);
+    }
+
+    useEffect(()=>{
+        getQuotes();
+    } , [])
+
   return (
-    <div>AllQuotes</div>
+    <div>
+        <h1>AllQuotes</h1>
+        <ul>
+
+        {
+            quotes.map((quote , index)=>{
+                return <Quote
+                    key={quote._id}
+                    author={quote.author}
+                    text={quote.text}
+                    id={quote._id}
+                 />
+            })
+        }
+
+        </ul>
+    </div>
+
   )
 }
 
-export default AllQuotes
+export default AllQuote
